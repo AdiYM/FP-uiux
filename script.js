9bb161adb96524fb098236b4b7af5cae628e4d37
AdiYM <adirangga565@gmail.com>
2024-07-18 22:40:40 +0700

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+// Ketika pengguna menggulir ke bawah 20px dari atas dokumen, tampilkan tombol
+
+//function pop up
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
